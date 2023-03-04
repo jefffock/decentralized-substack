5c067d6d3561ec90073127e5e54a5140eef0ffd3
spacebuneth <judekmartinez1997@gmail.com>
2023-03-03 21:59:38 -0500

    added new logo and logo font .png, edited page layout discover.jsx, edited Readme.md mission statement

--- packages/nextjs/pages/discover.jsx
+++ packages/nextjs/pages/discover.jsx
@@ -6,11 +6,12 @@ const Discover = () => {
     <>
     <NavBar />
     <div>Discover Authors</div>
+    <div className="flex justify-center">
     <div className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
-      <h3 className="text-base font-semibold leading-6 text-gray-900">Job Postings</h3>
+      <h3 className="text-base font-semibold leading-6 text-gray-900">Discover Authors</h3>
       <AuthorList />
     </div>
-    
+    </div>
     </>
   );
 };
