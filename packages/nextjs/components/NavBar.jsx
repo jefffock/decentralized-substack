e901d8972f2919f06b7e996d0dcbe6f46e46e7b5
Jeff Fox <jeffaveryfox@gmail.com>
2023-03-04 11:56:37 -0700

    Changed navbar to show active page

--- packages/nextjs/components/NavBar.jsx
+++ packages/nextjs/components/NavBar.jsx
@@ -12,7 +12,7 @@ export default function NavBar({ activePage }) {
     // <Disclosure as="nav" className="bg-white shadow">
     //   {({ open }) => (
     <>
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 pb-10">
         <div className="relative flex h-16 justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
