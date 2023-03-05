07266c600afab690caa63d6330a54df4d1429c12
Bhavya Agarwal <bhavya.agarwal@robinhood.com>
2023-03-04 21:15:06 -0700

    updating the authors list contract

--- packages/hardhat/contracts/AuthorsContract.sol
+++ packages/hardhat/contracts/AuthorsContract.sol
@@ -1,19 +1,16 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
-
 contract AuthorsListContract {
 
 // State Variables
-mapping(string => address) public subscriberMap;
+mapping(address => address) public subscriberMap;
 
 /**
  * Function that adds an author to the list of all authors
  *
  */
-function addAuthor(string memory authorName, address authorContractAddress) public{
+function addAuthor(address authorName, address authorContractAddress) public{
     subscriberMap[authorName] = authorContractAddress;
 }
 }
