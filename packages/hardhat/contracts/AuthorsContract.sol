397416d06bafa177d883d9a637ac0139332f5c53
Bhavya Agarwal <bhavya.agarwal@robinhood.com>
2023-03-04 21:15:37 -0700

    fixing tabs

--- packages/hardhat/contracts/AuthorsContract.sol
+++ packages/hardhat/contracts/AuthorsContract.sol
@@ -3,14 +3,14 @@ pragma solidity >=0.8.0 <0.9.0;
 
 contract AuthorsListContract {
 
-// State Variables
-mapping(address => address) public subscriberMap;
+    // State Variables
+    mapping(address => address) public subscriberMap;
 
-/**
- * Function that adds an author to the list of all authors
- *
- */
-function addAuthor(address authorName, address authorContractAddress) public{
-    subscriberMap[authorName] = authorContractAddress;
-}
+    /**
+    * Function that adds an author to the list of all authors
+    *
+    */
+    function addAuthor(address authorName, address authorContractAddress) public{
+        subscriberMap[authorName] = authorContractAddress;
+    }
 }
